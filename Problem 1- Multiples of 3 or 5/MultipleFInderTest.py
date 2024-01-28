bcad06967c24a0ce5ba60aaaea6d2eb493dfecb9
Sriram Ramesh <sramesh5@ucsc.edu>
2024-01-27 21:34:09 -0800

    Fix problem 1 solution to consider double counting

--- Problem 1- Multiples of 3 or 5/MultipleFInderTest.py
+++ Problem 1- Multiples of 3 or 5/MultipleFInderTest.py
@@ -21,5 +21,17 @@ class TestStringMethods(unittest.TestCase):
     def test_35_boundIsPrimePlus1(self):
         self.assertEqual(mf.sumMultiples(14, [3,5]), 45)
 
+    def test_23_inclusionExclusionPrincipleSimple(self):
+        self.assertEqual(mf.sumMultiples(7, [3,2]), 15)
+
+    def test_23_inclusionExclusionPrinciple(self):
+        self.assertEqual(mf.sumMultiples(15, [3,2]), 68)
+        # 2, 3, 4, 6, 8, 9, 10, 12, 14 = 68
+
+    def test_35_1000(self):
+        answer = mf.sumMultiples(1000, [3,5])
+        print(answer)
+        self.assertEqual(answer, 233168)
+
 if __name__ == '__main__':
     unittest.main()
--- Problem 1- Multiples of 3 or 5/MultipleFinder.py
+++ Problem 1- Multiples of 3 or 5/MultipleFinder.py
@@ -1,14 +1,25 @@
 import numpy as np
+import math
 
 # sum^(floor(n/3))_(i=1)(3i) + sum^(floor(n/5))_(i=1)(5i)  = 
 #  3/2((floor(n/3))^2 + floor(n/3)) + 5/2((floor(floor(n/5))) + floor(n/5))  =
 
 def sumMultiples(max: int, factorList: list) -> int :
     print("beginning sumMultiples for args", max, factorList)
+    if len(factorList) > 2:
+        print("Please limit size of factorList to 2") 
+        return -1
     factorArr = np.asarray(factorList)
     print("created factorArr ", factorArr)
     maxMultipleArr = np.floor((max-1)/factorArr)
     print("created maxMultipleArr ", maxMultipleArr)
     sumTermArr = factorArr * (((maxMultipleArr)**2) + (maxMultipleArr))/2
     print("created sumTermArr ", sumTermArr)
-    return sumTermArr.sum()
+    
+    excludeFactor = factorList[0]*factorList[1]
+    print("created excludeFactor: ", excludeFactor)
+    excludeMaxMultiple = math.floor((max-1)/excludeFactor)
+    excludeTerm = excludeFactor * ((excludeMaxMultiple**2) + (excludeMaxMultiple))/2
+    print("final excludeTerm ", excludeTerm)
+
+    return sumTermArr.sum() - excludeTerm
